2a987659766b026532e3295d9d070b35c3a5775c
Valeria De Los Angeles <145016866+valeriadelosangeles7@users.noreply.github.com>
2025-04-25 19:41:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function calculaTempo(tempoObjetivo) {
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[350];
         document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
         document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
